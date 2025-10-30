1ccec6ea5a1d8a1c8fa74a6596c7f652f3e8fc36
Notwebmaker <143799102+Notwebmaker@users.noreply.github.com>
2025-10-30 14:38:47 +1100

    actual game making
    making the actual game currently only one challange

--- navigation game.py
+++ navigation game.py
@@ -1,22 +1,33 @@
-import tkinter as tk
+from tkinter import*
+from tkinter.ttk import*
 import tkinter.messagebox
 
 def gotem():
     tkinter.messagebox.showinfo("You are an idiot.",  "HAHAHAHA! ")    
     tkinter.messagebox.showinfo("You are an idiot.",  "this is not the start of the game!  ")
-def winner():
-    tkinter.messagebox.showinfo("'insert happy music'", "you won! (this game isnt finished yet)")
 
-#root = Tk(screenName=None, baseName=None, className='Tk', useTk=1)
-w=tk.Tk()
-w.geometry("640x480")
-w.title("adventure game (stub)")
-y="Start"
-label = tk.Label(text=" a stupid adventure game. navigate to the end!(not yet complete)")
-label.pack()
-button = tk.Button(w, text=y, width=30,height=4,command=gotem)
-winnerbutton = tk.Button(w, text="close game", width=10,command=winner)
+def youwin():
+    tkinter.messagebox.showinfo("congratulations","You won!(this was easy right?)")
+    w.destroy()
 
-winnerbutton.pack(side=tk.BOTTOM)
-button.pack()
-w.mainloop() 
+w = Tk()
+w.geometry("640x480")  
+w.title("stupid navigation game")
+
+def nextchallange1():
+    tkinter.messagebox.showinfo("'insert happy music'", "next challange( is very simple)")
+    lvl1 = Toplevel(w)
+    lvl1.title("Challange 1")
+    lvl1.geometry("640x480")  
+    Label(lvl1, text="At least 1 button is lying, at least 1 speaks the truth").pack(pady=20)
+    Button(lvl1, text="you win if you click this button",command= w.destroy).pack(pady=20)
+    button2= Button(lvl1, text="the first button is lying, it is me", command=youwin).pack(pady=30)
+    wisebutton=Button(lvl1, text="i speak the truth, and i tell you one of the other two is lying, ", command=w.destroy).pack(pady=40)
+
+Label(w, text="A stupid adventure game, navigate to the end!").pack(pady=10)
+Button(w, text="Start",width=32, command=gotem).pack(side=TOP,pady=30)
+Button(w, text="close game", command=nextchallange1).pack(side=BOTTOM)
+
+
+# Run the Tkinter event loop
+w.mainloop()
